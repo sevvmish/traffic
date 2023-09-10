4e20ed0e800188e30130696f204f1be56081a2cf
Krivtsov <sevvmish@gmail.com>
2023-09-10 17:23:50 +0300

    10-09-2023

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Globals : MonoBehaviour
 {
     public const float TIME_FOR_ACCIDENT = 3f;
-    public const float DISTANCE_FOR_ACCIDENT = 1.5f;
+    public const float DISTANCE_FOR_ACCIDENT = 1.25f;
     public const float DISTANCE_FOR_OBJECTS = 1.5f;
     public static bool IsShowBorder = true;
 
-    public static float TIME_FOR_RIDE_TAXI = 3.5f;
+    public static float TIME_FOR_RIDE_TAXI = 4f;
     public static float TIME_FOR_RIDE_AMBULANCE = 4.5f;
-    public static float TIME_FOR_RIDE_VAN = 4.5f;
+    public static float TIME_FOR_RIDE_VAN = 5f;
 }
